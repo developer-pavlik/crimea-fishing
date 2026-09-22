c91feb96025078362bbaf63eab3ef390728995c4
Pavel Dzekunov <developer.pavlik@gmail.com>
2026-09-22 11:54:25 +0300

    Фикс табуляции

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div>Тут будет наш новый сайт</div>
-      </main>
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={100}
+                    height={20}
+                    priority
+                />
+                <div>Тут будет наш новый сайт</div>
+            </main>     
+        </div>
+    );
 }
