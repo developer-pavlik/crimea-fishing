ba512d4c2f483f014555444f8a40829fa613a01e
Pavel Dzekunov <developer.pavlik@gmail.com>
2026-09-23 10:53:56 +0300

    промежуточный

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../../public/logo.png';
 import styles from './Header.module.scss'
 
 const Header = () => (
     <header className={styles.header}>
         <div className='container'>
             <div className={styles.inner}>
+                <Link className={styles.logo} href="/">
+                    <Image
+                    src={logo}
+                    alt="Крымская рыбалка"
+                    className={styles.logoImg}
+                    priority
+                    />
+                </Link>
                 тест
             </div>
         </div>
