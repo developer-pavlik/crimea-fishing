bccadc6a5e9c3e12aa21400f9bd6dc1e10d94054
Pavel Dzekunov <developer.pavlik@gmail.com>
2026-09-23 10:19:15 +0300

    Промежуточные фиксы

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <div className='container'>
+            <div className={styles.inner}>
+                тест
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.scss'
+
+const Header = () => (
+    <header className={styles.header}>
+        <div className='container'>
+            <div className={styles.inner}>
+                тест
+            </div>
+        </div>
+    </header>
+);
+
+export default Header
