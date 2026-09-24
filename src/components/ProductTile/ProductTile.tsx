3b907bfa3f655a96ef52e3c820d0081eb8fa6c93
Pavel Dzekunov <developer.pavlik@gmail.com>
2026-09-24 11:20:54 +0300

    Промежуточный фикс

--- src/components/ProductTile/ProductTile.tsx
+++ src/components/ProductTile/ProductTile.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Image from "next/image";
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import Link from "next/link";
 import cn from "clsx";
 import styles from "./ProductTile.module.scss";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export interface ProductTileProps {
     title: string;
@@ -22,11 +29,19 @@ const ProductTile = ({
         <Link className={cn(styles.productTile, className)} href={href}>
             {imagesUrl[0] && (
                 <div className={styles.imageWrapper}>
-                    {/* <Image
-                        className={styles.image}
-                        src={imagesUrl[0]}
-                        alt={title}
-                    /> */}
+                    <Swiper
+                        className={styles.slider}                  
+                        modules={[Pagination]}
+                        pagination={{ clickable: false }}
+                        slidesPerView={1}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        >
+                        <SwiperSlide>Slide 1</SwiperSlide>
+                        <SwiperSlide>Slide 2</SwiperSlide>
+                        <SwiperSlide>Slide 3</SwiperSlide>
+                        <SwiperSlide>Slide 4</SwiperSlide>
+                    </Swiper>
                 </div>
             )}
             <div className={styles.content}>
